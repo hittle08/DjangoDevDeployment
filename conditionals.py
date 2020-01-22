e643dddb4b8dfa51fc17124a7001838b49ea4261
hittle08 <hittle08@gmail.com>
2020-01-22 11:10:32 -0500

    completed

--- conditionals.py
+++ conditionals.py
@@ -1,14 +1,47 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+x = 6
+y = 10
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+#simple if
+# if x  ==  y: 
+#     print (f'{x} is equal to {y}')
+
+#if condition
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# else:
+#     print(f'{x} is less than {y}')
+
+# #elif
+# if x > y:
+#     print(f'{x} is greater than {y}')
+# elif x==y:
+#     print(f'{x} is equal to {y}')
+# else:
+#     print(f'{x} is less thatn {y}')
+    
+#nested if (Note: This is not a proper way to write. Use and an operater in the next section.)
+# if x > 2:
+#     if x <= 10:
+#         print(f'{x} is less than 2 and greather than 10')
 
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
+#and 
+if x > 2 and x <= 10:
+    print(f'{x} is less than 2 and greather than 10')
 
+#or
+if x > 2 or x<=10:
+    print(f'{x} is less than 2 or greater than 10')
+
+#not 
+if not(x == y):
+    print(f'{x} is not equal to {y}')
 
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
